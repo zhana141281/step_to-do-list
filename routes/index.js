f1b5903e21c22af088bef4f8e46dad6f9647f47a
@zhana141281 <zhana141281@gmail.com>
2019-09-13 16:46:39 +0300

    Bases for back-end and

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+let collection;
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:admin@cluster0-2uqkx.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+client.connect(err => {
+  console.log('DB Connection error:', err);
+  collection = client.db("todo").collection("todo");
+});
+
+/* GET home page. */
+router.get('/',async function(req, res, next) {
+  const data = await collection.find({});
+  let lists =[];
+  await data.forEach(item=>{
+    lists.push(item);
+  });
+  console.log(lists);
+  res.render('index', {lists});
+});
+
+module.exports = router;
