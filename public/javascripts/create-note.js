98df84248484f857b4e75856cd1e10ff0ef59874
Fetisova Tetiana <tbabich3003@gmail.com>
2019-09-17 16:42:39 +0300

    adding functions for edit and delete functions

--- public/javascripts/create-note.js
+++ public/javascripts/create-note.js
@@ -33,14 +33,84 @@ if(document.querySelector('#create-form')){
             'POST',
             {
                 ...data,
-                id: Date.now()
+                id: Date.now(),
+                type:'note',
             },
             cardCreatedHandler
         )
 
     });
 }
-async function customFetch(endpoint,method, body, callback) {
+if(document.querySelector('#edit-popup form')) {
+    document.querySelector('#edit-popup form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        let data = collectFormData(this);
+        console.log(data);
+        customFetch(`/api/notes/${data.id}`,
+            'PUT',
+            {
+                ...data,
+                type: 'note'
+            }
+        );
+        hidePopup();
+        document.querySelector('.card.card-details').style.backgroundColor = data.color;
+        document.querySelector('.card-title').innerText = data.title;
+        document.querySelector('.card-text').innerText = data.text;
+        checkColor();
+});
+}
+if(document.getElementById('single-note-page')) {
+    checkColor();
+    document.querySelector('[data-edit]').addEventListener('click', (e) => {
+        editBtnHandler(e.target.closest('[data-edit]').dataset.edit)
+    });
+
+    document.querySelector('[data-delete]').addEventListener('click', (e) => {
+        customFetch(
+            `/api/notes/${+e.target.closest('[data-delete]').dataset.delete}`,
+            'DELETE'
+        );
+        window.location = '/';
+    })
+
+}
+function checkColor() {
+    let cardDetails = document.querySelector('.card.card-details');
+    if(cardDetails.style.backgroundColor===null||cardDetails.style.backgroundColor==='transparent' ){
+        cardDetails.style.border = '1px solid #e5e5e5'
+    }else{
+        cardDetails.style.border = 'none'
+    }
+
+}
+function editBtnHandler(id) {
+    const popup = document.getElementById('edit-popup');
+    const card = document.querySelector(`[data-id="${id}"]`);
+    console.log('card',card);
+    console.log('card.style.backgroundColor',card.style.backgroundColor);
+
+    popup.querySelector('[name="title"]').value = card.querySelector('h5').innerText;
+    popup.querySelector('[name="text"]').value = card.querySelector('p').innerText;
+    popup.querySelector('[name="id"]').value = id;
+    popup.querySelector('[name="color"]').value = card.style.backgroundColor;
+    showPopup();
+}
+document.querySelector('#edit-popup').addEventListener('click', (e)=> {
+        if(e.target.classList.contains('popup')) {
+            hidePopup()
+        }
+    });
+
+function showPopup() {
+    const popup = document.getElementById('edit-popup');
+    popup.style.display = 'block';
+}
+function hidePopup() {
+    const popup = document.getElementById('edit-popup');
+    popup.style.display = 'none';
+}
+async function customFetch(endpoint,method, body) {
     const response = await fetch(endpoint, {
         method: method,
         headers: {
@@ -53,7 +123,7 @@ async function customFetch(endpoint,method, body, callback) {
     const answer = JSON.parse(json);
     console.log('answer',answer);
     if(answer.status){
-        callback(body)
+        console.log(body);
     }else{
         alert('Sorry, something went wrong... :(')
     }
