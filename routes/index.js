8663f8336124d06e98682b3d8840196fa3121f5e
OlgaVakulenko <55438875+OlgaVakulenko@users.noreply.github.com>
2019-09-18 12:39:28 +0300

    Developer (#3)
    * html for create note + create note in database function added
    
    * Added basic disign for Home Page
    
    * card cteate route plus cald-details view added
    
    * note view in progress
    
    * added new styles for Hime page mobile size
    
    * Home page (#1)
    
    * html for create note + create note in database function added
    
    * card cteate route plus cald-details view added
    
    * note view in progress
    
    * added new styles for Hime page mobile size
    
    * Tiny changes in the style
    
    * adding functions for edit and delete functions
    
    * adding functions for edit and delete functions (#2)
    
    * final corrections

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,52 @@ router.get('/',async function(req, res, next) {
   res.render('index', {lists});
 });
 
+/*Note creation route*/
+router.get('/notes', (req, res)=>{
+    res.render('create-note')
+});
+
+//create note
+router.post('/api/notes', async (req, res, next)=> {
+    console.log('req.body', req.body);
+    const {id, title, text, color, type} = req.body;
+    const data = await collection.insertOne({id, title, text, color, type});
+    res.json(JSON.stringify({
+        status: !!data.insertedId
+    }))
+});
+
+//note details page
+router.get('/notes/:id', async (req, res, next)=> {
+    const id = +req.params.id;
+    const note = await collection.findOne({id});
+    res.render('note', { note });
+});
+
+//edit note page
+router.put('/api/notes/:id', async(req,res,next)=>{
+    const id = +req.params.id;
+    const {title, text, color, type} = req.body;
+    const data = await collection.updateOne(
+        {id: +id},
+        {$set: {title, text, color, type}}
+    );
+    res.json(JSON.stringify({
+        status: !!data.modifiedCount
+    }))
+
+});
+//delete note
+router.delete('/api/notes/:id', async(req, res, next) => {
+    const id = req.params.id;
+    console.log(' req.params.id',id);
+
+    const data = await collection.deleteOne({id: +id});
+    res.json(JSON.stringify({
+        status: !!data.deletedCount
+    }))
+
+});
 router.get('/lists', (req, res, next) => {
   res.render('lists');
 });
