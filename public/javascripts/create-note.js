0bda7e983ce513d99eef95959a8ef632d1d90630
zhana141281 <53611771+zhana141281@users.noreply.github.com>
2019-09-24 16:51:31 +0300

    Developer (#6)
    * html for create note + create note in database function added
    
    * Added basic disign for Home Page
    
    * card cteate route plus cald-details view added
    
    * note view in progress
    
    * created list route
    
    * added new styles for Hime page mobile size
    
    * Home page (#1)
    
    * html for create note + create note in database function added
    
    * card cteate route plus cald-details view added
    
    * note view in progress
    
    * added new styles for Hime page mobile size
    
    * Tiny changes in the style
    
    * adding functions for edit and delete functions
    
    * adding functions for edit and delete functions (#2)
    
    * final corrections
    
    * create list
    
    * Add list-views
    
    * New bootsprap styles
    
    * Created Readme file
    
    added readme file
    
    * List (#4)
    
    * Developer (#3)
    
    * html for create note + create note in database function added
    
    * Added basic disign for Home Page
    
    * card cteate route plus cald-details view added
    
    * note view in progress
    
    * added new styles for Hime page mobile size
    
    * Home page (#1)
    
    * html for create note + create note in database function added
    
    * card cteate route plus cald-details view added
    
    * note view in progress
    
    * added new styles for Hime page mobile size
    
    * Tiny changes in the style
    
    * adding functions for edit and delete functions
    
    * adding functions for edit and delete functions (#2)
    
    * final corrections
    
    * edit button handler function
    
    * create render edit list, fixed buttons
    
    * create logic of render list
    
    * final version with fixed buttons bug
    
    * List2 (#5)
    
    * Developer (#3)
    
    * html for create note + create note in database function added
    
    * Added basic disign for Home Page
    
    * card cteate route plus cald-details view added
    
    * note view in progress
    
    * added new styles for Hime page mobile size
    
    * Home page (#1)
    
    * html for create note + create note in database function added
    
    * card cteate route plus cald-details view added
    
    * note view in progress
    
    * added new styles for Hime page mobile size
    
    * Tiny changes in the style
    
    * adding functions for edit and delete functions
    
    * adding functions for edit and delete functions (#2)
    
    * final corrections
    
    * edit button handler function
    
    * create render edit list, fixed buttons
    
    * create logic of render list
    
    * final version with fixed buttons bug
    
    * fixed delete button on edited list
    
    * fixed buttons bug

---
+++ public/javascripts/create-note.js
@@ -0,0 +1,156 @@
+const colorPicker = document.querySelector('#color-picker');
+const colorMenu = document.querySelector('#color-menu');
+const noteBgInput = document.querySelector('.note-bg-color');
+const colors = document.querySelectorAll('.color');
+
+colorPicker.addEventListener('click', (e) => {
+    if (!colorMenu.classList.contains('active-menu')) {
+        colorMenu.classList.add('active-menu');
+    } else {
+        colorMenu.classList.remove('active-menu');
+    }
+});
+document.body.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('color-menu') && !e.target.classList.contains('color-picker')) {
+        colorMenu.classList.remove('active-menu');
+    }
+});
+colors.forEach((el) => {
+    el.addEventListener('click', (e) => {
+        let bgColor = el.dataset.color;
+        noteBgInput.dataset.color = bgColor;
+        noteBgInput.value = bgColor;
+        const pickedColor = document.querySelector('.picked-color');
+        pickedColor.style.backgroundColor = bgColor;
+    })
+});
+if (document.querySelector('#create-form')) {
+    document.querySelector('#create-form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        let data = collectFormData(this);
+        customFetch('/api/notes',
+            'POST',
+            {
+                ...data,
+                id: Date.now(),
+                type: 'note',
+            },
+            cardCreatedHandler
+        );
+    });
+}
+if (document.querySelector('#edit-popup form')) {
+    document.querySelector('#edit-popup form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        let data = collectFormData(this);
+        console.log(data);
+        customFetch(`/api/notes/${data.id}`,
+            'PUT',
+            {
+                ...data,
+                type: 'note'
+            },
+            hidePopup
+        );
+
+        document.querySelector('.card.card-details').style.backgroundColor = data.color;
+        document.querySelector('.card-title').innerText = data.title;
+        document.querySelector('.card-text').innerText = data.text;
+        checkColor();
+    });
+}
+if (document.getElementById('single-note-page')) {
+    checkColor();
+    document.querySelector('#edit-popup').addEventListener('click', (e) => {
+        if (e.target.classList.contains('popup')) {
+            hidePopup()
+        }
+    });
+    document.querySelector('[data-edit]').addEventListener('click', (e) => {
+        editBtnHandler(e.target.closest('[data-edit]').dataset.edit)
+    });
+
+    document.querySelector('[data-delete]').addEventListener('click', (e) => {
+        let deleteID = e.target.closest('[data-delete]').dataset.delete;
+        deleteFetch(
+            `/api/notes/${deleteID}`,
+            'DELETE'
+        );
+
+    })
+
+}
+function checkColor() {
+    let cardDetails = document.querySelector('.card.card-details');
+    if (cardDetails.style.backgroundColor === null || cardDetails.style.backgroundColor === 'transparent') {
+        cardDetails.style.border = '1px solid #e5e5e5'
+    } else {
+        cardDetails.style.border = 'none'
+    }
+
+}
+function editBtnHandler(id) {
+    const popup = document.getElementById('edit-popup');
+    const card = document.querySelector(`[data-id="${id}"]`);
+    console.log('card', card);
+    console.log('card.style.backgroundColor', card.style.backgroundColor);
+    popup.querySelector('[name="title"]').value = card.querySelector('h5').innerText;
+    popup.querySelector('[name="text"]').value = card.querySelector('p').innerText;
+    popup.querySelector('[name="id"]').value = id;
+    popup.querySelector('[name="color"]').value = card.style.backgroundColor;
+    showPopup();
+}
+function showPopup() {
+    const popup = document.getElementById('edit-popup');
+    popup.style.display = 'block';
+}
+function hidePopup() {
+    const popup = document.getElementById('edit-popup');
+    popup.style.display = 'none';
+}
+async function customFetch(endpoint, method, body, callback) {
+    const response = await fetch(endpoint, {
+        method: method,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+    const json = await response.json();
+    const answer = JSON.parse(json);
+    console.log('answer', answer);
+    if (answer.status) {
+        console.log(body);
+        callback();
+    } else {
+        alert('Sorry, something went wrong... :(')
+    }
+}
+async function deleteFetch(endpoint, method) {
+    const response = await fetch(endpoint, {
+        method: method,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
+    const json = await response.json();
+    const answer = JSON.parse(json);
+    console.log('answer', answer);
+    if (answer.status) {
+        window.location = '/';
+    } else {
+        alert('Sorry, something went wrong... :(')
+    }
+}
+function collectFormData(el) {
+    let data = {};
+    el.querySelectorAll('input, textarea').forEach(field => {
+        data[field.name] = field.value;
+    });
+    return data;
+}
+function cardCreatedHandler() {
+    window.location = '/';
+}
--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,101 @@ router.get('/',async function(req, res, next) {
   res.render('index', {lists});
 });
 
+/*Note creation route*/
+router.get('/notes', (req, res)=>{
+    res.render('create-note')
+});
+
+//create note
+router.post('/api/notes', async (req, res, next)=> {
+    console.log('req.body', req.body);
+    const {id, title, text, color, type} = req.body;
+    const data = await collection.insertOne({id, title, text, color, type});
+    res.json(JSON.stringify({
+        status: !!data.insertedId
+    }))
+});
+
+//note details page
+router.get('/notes/:id', async (req, res, next)=> {
+    const id = +req.params.id;
+    const note = await collection.findOne({id});
+    res.render('note', { note });
+});
+
+//edit note page
+router.put('/api/notes/:id', async(req,res,next)=>{
+    const id = +req.params.id;
+    const {title, text, color, type} = req.body;
+    const data = await collection.updateOne(
+        {id: +id},
+        {$set: {title, text, color, type}}
+    );
+    res.json(JSON.stringify({
+        status: !!data.modifiedCount
+    }))
+
+});
+//delete note
+router.delete('/api/notes/:id', async(req, res, next) => {
+    const id = req.params.id;
+    console.log(' req.params.id',id);
+
+    const data = await collection.deleteOne({id: +id});
+    res.json(JSON.stringify({
+        status: !!data.deletedCount
+    }))
+
+});
+router.get('/lists', (req, res, next) => {
+  res.render('lists');
+});
+
+//Create list
+router.post('/api/lists', async(req, res, next) => {
+  console.log(req.body);
+  const {id, title, lists, type} = req.body;
+
+  const data = await collection.insertOne({id, title, lists, type});
+  res.json(JSON.stringify({
+    status: !!data.insertedId
+  }))
+
+});
+
+// Render single list
+router.get('/lists/:id', async(req, res, next) => {
+    const id = +req.params.id;
+    const toDos = await collection.findOne({id});
+    res.render('list', {toDos});
+});
+
+// *Delete list
+router.delete('/api/lists/:id', async(req, res, next) => {
+    const id = req.params.id;
+
+    const data = await collection.deleteOne({id: +id});
+    res.json(JSON.stringify({
+        status: !!data.deletedCount
+    }))
+
+});
+
+// Edit list
+router.put('/api/lists/:id', async(req, res, next) => {
+    const id = +req.params.id;
+    const {title, lists, type} = req.body;
+    console.log(req.body);
+    console.log(lists);
+    const data = await collection.updateOne(
+        {id: +id},
+        {$set: {title, lists, type}},
+    );
+    res.json(JSON.stringify({
+        status: !!data.modifiedCount
+    }))
+
+});
+
+
 module.exports = router;
